dcd3d227f4e9d932d4ca499a2720fbde80c5391b
alexey kozlenkov <nidecker95@gmail.com>
2014-07-24 11:14:02 +0300

    Add transactional annotations to DocumentServiceImpl, so, now delete & update document operations are correct

--- src/main/java/com/exadel/studbase/web/service/impl/DocumentServiceImpl.java
+++ src/main/java/com/exadel/studbase/web/service/impl/DocumentServiceImpl.java
@@ -5,6 +5,8 @@ import com.exadel.studbase.web.domain.document.Document;
 import com.exadel.studbase.web.service.IDocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -18,21 +20,25 @@ public class DocumentServiceImpl implements IDocumentService {
     private IDocumentDAO documentDAO;
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public Document save(Document document) {
         return documentDAO.saveOrUpdate(document);
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public Document getById(Long id) {
         return documentDAO.find(id);
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public void delete(Document document) {
         documentDAO.delete(document);
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public Collection<Document> getAllForUser(Long id) {
         return documentDAO.getAllForUser(id);
     }
