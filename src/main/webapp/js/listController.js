61d8cdbde126e9772fdd4414a77c348769982a11
ala'n <alan.lonoi@yandex.ru>
2014-07-27 14:06:00 +0300

    Filter front-end integration, ListPage design changed:   - table-striped order fixed!   - button size changed   - popup deleted, filter will be inline   - info label set as footer
      TODO: color split, correct css styles, write front-end controller:
      filter-core, filter-design-control.

--- src/main/webapp/js/listController.js
+++ src/main/webapp/js/listController.js
@@ -1,7 +1,7 @@
 /**
  * Created by ala'n on 14.07.2014.
  */
-$(window).ready(function(){
+$(window).ready(function () {
     checkEmptyFieldSize();
 });
 $(document).ready(function () {
@@ -33,6 +33,23 @@ function bindEventControl() {
     $("#startSearchButton").click(function () {
         loadTable();
         updateInfoLabel();
+    });
+
+    $("#menu").mouseleave(
+        function () {
+            $('#menu').hide(400);
+        }
+    );
+
+    $("#addFilterButton").click(function () {
+        addFilterAction();
+    });
+
+
+    $("#searchLine").focus(function () {
+        $(this).animate({ width: "250pt"}, 1000);
+    }).blur(function () {
+        $(this).animate({ width: "150pt"}, 500);
     });
 }
 
@@ -70,22 +87,24 @@ function showFilter() {
 }
 ////////////////////////////////////// Toggle popup ////////////////////////////////////////////
 function togglePopup(popup) {
-    if(popup.is(':visible')){
+    if (popup.is(':visible')) {
         popup.hide();
-    }else{
+        return false;
+    } else {
         popup.show();
+        return true;
     }
 }
-function centrePopup(popup,x, y){
+function centrePopup(popup, x, y) {
     var WIDTH = $(window).width();
     var HEIGHT = $(window).height();
     var popupWidth = popup.width();
     var popupHeight = popup.height();
-    if(x == undefined || x == null){
-        x = (WIDTH - popupWidth)/2;
+    if (x == undefined || x == null) {
+        x = (WIDTH - popupWidth) / 2;
     }
-    if(y == undefined || y == null){
-        y = (HEIGHT - popupHeight)/2;
+    if (y == undefined || y == null) {
+        y = (HEIGHT - popupHeight) / 2;
     }
     popup.css({
         left: x,
@@ -106,7 +125,7 @@ function getCheckedRowsId() {
 };
 function onCheckedAll() {
     var globalChecked = $("#checkAll").prop("checked");
-    $('.item-checkbox').each(function(){
+    $('.item-checkbox').each(function () {
         this.checked = globalChecked;
     });
 }
@@ -170,11 +189,11 @@ function clearTable() {
     $("#studTable > tbody").empty();
 }
 ///////////////////////////////////// Table Subutil ////////////////////////////////////////////
-function resizeControl(){
+function resizeControl() {
     var HEIGHT = $("#headerBlock").outerHeight();
     $(window).resize(function () {
         var localHeight = $("#headerBlock").outerHeight();
-        if(HEIGHT != localHeight) {
+        if (HEIGHT != localHeight) {
             HEIGHT = localHeight;
             checkEmptyFieldSize();
         }
@@ -210,4 +229,94 @@ function updateInfoLabel() {
         $("#infoLabel").text("-------------------- List is empty --------------------");
     }
 }
+
+//////////////////////////////////////// Filter ////////////////////////////////////////////////
+var counter = 1;
+var filterTypes = {
+    changed: true,
+    keys: ['age', 'enum', 'role', 'name'],
+    keyMap: {
+        'age': null,
+        'enum': ['first', 'second', 'third'],
+        'role': ['admin', 'user', 'guest'],
+        'name': null
+    }
+};
+
+//Menu Listener body
+function menuEvent(name) {
+    $("#menu").hide();
+    addFilterAttribute(counter++, name, filterTypes.keyMap[name]);
+}
+
+function addFilterAction() {
+    var $menu = $("#menu");
+    if ($menu.is(':visible')) {
+        $menu.hide(300);
+    } else {
+        setPositionrevilTo($menu, $("#addFilterButton"));
+        if (filterTypes.changed) {
+            filterTypes.changed = false;
+            clearMenu($menu);
+            fillMenu($menu, filterTypes.keys);
+        }
+        $('#menu').animate({ opacity: 'toggle', height: 'toggle'}, 300);
+    }
+}
+function addFilterAttribute(id, name, values) {
+    var htmContent = "";
+    htmContent += "<div id = 'filter_" + id + "' class='btn-group input-group item-btn-group filter-itm'>";
+
+    htmContent += "<button class='btn btn-gray item-btn-attr' onclick='";
+    htmContent += "deleteFilter(" + id + ");";
+    htmContent += "'>";
+    htmContent += name;
+    htmContent += "</button>";
+
+    if (values) {
+        htmContent += "<select class='selectpicker' id='filter_" + id + "_select' style='width:60%'>";
+        values.forEach(function (value) {
+            htmContent += "<option>";
+            htmContent += value;
+            htmContent += "</option>";
+        });
+        htmContent += "</select>";
+    } else {
+        htmContent += "<input type='text' class='form-control value-field' value='some'>";
+    }
+    htmContent += "</div>";
+
+    $("#filterBlock").prepend(htmContent);
+}
+
+function deleteFilter(id) {
+    var el = $("#filter_" + id);
+    el.remove();
+}
+
+function clearMenu(menu) {
+    menu.empty();
+}
+function fillMenu(menu, data) {
+    var liCont = "";
+    for (var i = 0; i < data.length; i++) {
+        liCont += "<li class='menu-item' role='presentation'>" +
+            "<a onclick='menuEvent(\"" + data[i] + "\");' " +
+            "name='menu_itm' role='menuitem'>" + data[i] + "</a></li>";
+    }
+    menu.append(liCont);
+}
+
+function setPositionrevilTo(element, parent) {
+    var pos = parent.offset();
+    var x = pos.left;
+    var y = pos.top + parent.height();
+    element.css(
+        {
+            left: x,
+            top: y
+        }
+    );
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////
+
