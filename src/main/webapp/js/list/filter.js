e0bbe91b676a6877ed92391d95073be9ac0203a5
ala'n <alan.lonoi@yandex.ru>
2014-08-01 14:43:46 +0300

    Refactor and rebuild front-end code. Inject library (underscore-min.js). Renamed same classes, ids and other attributes of list page. Reset.css injected into common.
    !!!!!!!!!!!!!!!!!!!!!!!!!! NOT STABLE !!!!!!!!!!!!!!!!!!!!!!!!!

--- src/main/webapp/js/list/filter.js
+++ src/main/webapp/js/list/filter.js
@@ -3,127 +3,108 @@
  */
 
 var MAX_FILTER_COUNT = 10,
-    filterTypes = {
+    filter = {
         changed: true,
-        keys: ['age', 'working hour', 'billable', 'skill', 'english', 'date'],
-        keyType: {
-            'age': 'number',
-            'working hour': 'number',
-            'billable': 'date',
-            'skill': 'leveled-list',
-            'english': 'list',
-            'date': 'date'
-        },
-        keyMap: {
-            'skill': ['java', 'C++', '.NET', 'HTML', 'Mongo DB', 'SQL'],
-            'english': ['1', '2', '3', '4', '5']
-        },
-        multiType: {
-            'skill': true
-        }
-    },
-    SEPARATOR = "<span class='static-green filter-separator'>and</span>";
+        types: [
+            {
+                name: 'age',
+                type: 'number'
+            },
+            {
+                name: 'workingHours',
+                type: 'number'
+            },
+            {
+                name: 'billable',
+                type: 'date'
+            },
+            {
+                name: 'skill',
+                type: 'leveled-list',
+                values: ['java', 'C++', '.NET', 'HTML', 'Mongo DB', 'SQL'],
+                multiset: true
+            },
+            {
+                name: 'english',
+                type: 'list',
+                values: ['1', '2', '3', '4', '5']
+            },
+            {
+                name: 'date',
+                type: 'date'
+            }
+        ]
+    };
 
 
-$(window).ready(function(){
+$(window).ready(function () {
     $("#filterMenu").mouseleave(function () {
         $('#filterMenu').hide(400);
     });
     $("#addFilterButton").click(function () {
-        addFilterAction();
+        toggleFilterChooseMenu();
+    });
+    $("#filterMenu").on("click","a",function(){
+            $("#filterMenu").hide();
+            addFilterAttribute($(this).text());
+            checkFilterCount();
+    });
+    $("#filter").on("click",".filter-name-btn", function(){
+        var selfItem = $($(this).parent().get(0));
+        var prevItem = selfItem.prev();
+        var nextItem = selfItem.next();
+        var filterName = selfItem.attr("data-filter");
+        if (prevItem.is('.filter-separator')) {
+            prevItem.remove();
+        } else if (nextItem.is('.filter-separator')) {
+            nextItem.remove();
+        }
+        selfItem.remove();
+        $("#filterMenu > li[data-filter-name='" + filterName + "']").show();
+        checkFilterCount();
     });
 });
 
-function addFilterAttribute(name) {
-    var filterElementContent = createFilterAttributeContent(name);
-    if (filterTypes.multiType[name]) {
-        var filtersExist = existFilterAttribute(name);
-        if (filtersExist && filtersExist.length > 0) {
-            filtersExist.last().after(SEPARATOR + filterElementContent);
-        } else {
-            $("#addFilterButton").before(filterElementContent);
-        }
+function toggleFilterChooseMenu() {
+    var $menu = $("#filterMenu");
+    if ($menu.is(':visible')) {
+        $menu.hide(300);
     } else {
-        $("#addFilterButton").before(filterElementContent);
-        $("#filterMenu > li[data-filter-name='" + name + "']").hide();
+        setLocationRelativeTo($menu, $("#addFilterButton"));
+        if (filter.changed) {
+            filter.changed = false;
+            var filterMenuTemplate = Handlebars.compile($('#filterMenuTemplate').html());
+            $menu.empty();
+            $menu.append(filterMenuTemplate(filter));
+        }
+        $('#filterMenu').animate({ opacity: 'toggle', height: 'toggle'}, 300);
     }
 }
-function createFilterAttributeContent(name) {
-    var typeName = filterTypes.keyType[name];
-    var htmContent = "";
 
-    htmContent += "<div filter='" + name + "' class='btn-group input-group filter-item";
-    htmContent += " data-filter-" + typeName + "'>";
-    htmContent += "<button class='btn prj-btn remove-btn item-btn-attr' ";
-    htmContent += "onclick=\"removeFilterAttribute( '" + name + "', this );\">";
-    htmContent += name;
-    htmContent += "</button>";
-
-    switch (typeName) {
-        case 'number':
-            htmContent += "<input type='number' class='form-control value-field' placeholder='??'>";
-            break;
-        case 'date':
-            htmContent += "<input type='date' class='form-control value-field' placeholder='date'>";
-            break;
-        case 'list':
-            htmContent += "<select class='selectpicker value-field' style='width:60%'>";
-            filterTypes.keyMap[name].forEach(function (value) {
-                htmContent += "<option>";
-                htmContent += value;
-                htmContent += "</option>";
-            });
-            htmContent += "</select>";
-            break;
-        case 'leveled-list':
-            htmContent += "<select class='selectpicker value-field' style='width:50%'>";
-            filterTypes.keyMap[name].forEach(function (value) {
-                htmContent += "<option>";
-                htmContent += value;
-                htmContent += "</option>";
-            });
-            htmContent += "</select>";
-            htmContent += "<select class='selectpicker sub-value-field' style='width:40pt'>";
-            for (var level = 1; level <= 5; level++) {
-                htmContent += "<option>";
-                htmContent += level;
-                htmContent += "</option>";
+function addFilterAttribute(name) {
+    var filterElementTemplate = Handlebars.compile($('#filterTemplate').html());
+    var filterContext = _.find(filter.types, function(element){return element.name == name;});
+    if(filterContext) {
+        if (filterContext.multiset) {
+            var filterEqType = $(".filter-item[data-filter='" + name + "']");
+            if (filterEqType && filterEqType.length > 0) {
+                filterEqType.last().after(filterElementTemplate({name: name, separate: true}));
+            } else {
+                $("#addFilterButton").before(filterElementTemplate({name: name}));
             }
-            htmContent += "</select>";
-            break;
-        default :
-            console.error("Not defined filter type");
+        } else {
+            $("#addFilterButton").before(filterElementTemplate({name: name}));
+            $("#filterMenu > li[data-filter-name='" + name + "']").hide();
+        }
     }
-    htmContent += "</div>";
-    return htmContent;
-}
-
-function existFilterAttribute(name) {
-    return $(".filter-item[data-filter='" + name + "']");
-}
-function removeFilterAttribute(name, element) {
-    var selfItem = $($(element).parent().get(0));
-    var prevItem = selfItem.prev();
-    var nextItem = selfItem.next();
-    if (prevItem.is('.filter-separator')) {
-        prevItem.remove();
-    } else if (nextItem.is('.filter-separator')) {
-        nextItem.remove();
-    }
-
-    selfItem.remove();
-    $("#filterMenu > li[data-filter-name='" + name + "']").show();
-    checkFilterCount();
 }
 
 function checkFilterCount() {
     var count = $(".filter-item").length;
     var filterBtn = $("#addFilterButton");
     if (count < MAX_FILTER_COUNT) {
-        filterBtn.prev().show();
         filterBtn.show();
     } else {
-        filterBtn.prev().hide();
         filterBtn.hide();
     }
     checkContentMargin();
@@ -167,59 +148,4 @@ function pickFilters() {
     }
     ;
     return returnStatement;
-}
-
-
-//Menu Listener body
-function menuEvent(name) {
-    $("#filterMenu").hide();
-    addFilterAttribute(name);
-    checkFilterCount();
-}
-
-function addFilterAction() {
-    var $menu = $("#filterMenu");
-    if ($menu.is(':visible')) {
-        $menu.hide(300);
-    } else {
-        setLocationRelativeTo($menu, $("#addFilterButton"));
-        if (filterTypes.changed) {
-            filterTypes.changed = false;
-            clearMenu($menu);
-            fillMenu($menu, filterTypes.keys);
-        }
-        $('#filterMenu').animate({ opacity: 'toggle', height: 'toggle'}, 300);
-    }
-}
-
-function clearMenu(menu) {
-    menu.empty();
-}
-function fillMenu(menu, data) {
-    var liCont = "";
-    for (var i = 0; i < data.length; i++) {
-        liCont += "<li class='menu-item' data-filter-name='" + data[i] + "'>" +
-            "<a onclick='menuEvent(\"" + data[i] + "\");' role='menuitem'>" +
-            data[i] + "</a></li>";
-    }
-    menu.append(liCont);
-}
-
-function setLocationRelativeTo(element, parent) {
-    var winOffsetW = $(window).width();
-
-    var pos = parent.offset();
-
-    var x = pos.left;
-    var space = pos.left + element.width() - winOffsetW + 4;
-    if (space > 0)
-        x -= space;
-    var y = pos.top + parent.height();
-
-    element.css(
-        {
-            left: x,
-            top: y
-        }
-    );
 }
