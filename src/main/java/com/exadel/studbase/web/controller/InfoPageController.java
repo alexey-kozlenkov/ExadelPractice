76ac85b47626d237841139b78810dfcfdb73e9eb
alexey kozlenkov <nidecker95@gmail.com>
2014-08-04 00:02:01 +0300

    Final version of changes in InfoPageController.

--- src/main/java/com/exadel/studbase/web/controller/InfoPageController.java
+++ src/main/java/com/exadel/studbase/web/controller/InfoPageController.java
@@ -16,20 +16,15 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.Principal;
 import java.sql.Date;
-import java.util.Map;
-import java.util.Set;
 
 
 @Controller
 @Secured("IS_AUTHENTICATED_FULLY")
-@RequestMapping("/")
+@RequestMapping("/info")
 public class InfoPageController {
 
     @Autowired
@@ -49,7 +44,7 @@ public class InfoPageController {
     @Autowired
     ICuratoringDAO curatoringService;
 
-    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String infoPage(@RequestParam("id") Long id) {
         MySecurityUser principal = (MySecurityUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_STUDENT")) && !principal.getId().equals(id)) {
@@ -58,39 +53,27 @@ public class InfoPageController {
         return "studentInfo";
     }
 
-    @RequestMapping(value = "/info/getOptions", method = RequestMethod.GET)
+    @RequestMapping(value = "/getOptions", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public String studentData(HttpServletResponse response) {
-        //try {
+    public String studentData() {
             Gson gson = new Gson();
             Options options = new Options();
-            //response.getWriter().print(gson.toJson(options, Options.class));
             return gson.toJson(options, Options.class);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        //response.setStatus(200);
     }
 
-    @RequestMapping(value = "/info/getManualInformation", method = RequestMethod.GET)
+    @RequestMapping(value = "/getManualInformation", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public String manualData(@RequestParam("studentId") Long studentId, HttpServletResponse response) {
-        //try {
+    public String manualData(@RequestParam("studentId") Long studentId) {
             Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
             User user = userService.getById(studentId);
             System.out.println(user.getStudentInfo().toString());
-            //response.getWriter().print(gson.toJson(user, User.class));
         return gson.toJson(user, User.class);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        //response.setStatus(200);
     }
 
     @Secured({"ROLE_SUPERADMIN", "ROLE_OFFICE", "ROLE_STUDENT"})
-    @RequestMapping(value = "/info/postManualInformation", method = RequestMethod.POST)
+    @RequestMapping(value = "/postManualInformation", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String editManualInformation(@RequestParam("studentId") Long id,
@@ -99,22 +82,6 @@ public class InfoPageController {
                                         @RequestParam("studentPassword") String password,
                                         @RequestParam("studentEmail") String email,
                                         @RequestParam("studentState") String state) {
-//
-//            Map<String, String[]> params = request.getParameterMap();
-//            Set<Map.Entry<String, String[]>> entry = params.entrySet();
-//            for (Map.Entry<String, String[]> element : params.entrySet()) {
-//                System.out.print("Key = " + element.getKey()); //+ ", Value = " + element.getValue()[0]);
-//                for (String s : element.getValue())
-//                    System.out.print("\t" + s);
-//                System.out.println();
-//            }
-//            User editedUser = userService.getById(Long.parseLong(request.getParameter("studentId")));
-//            editedUser.setName(request.getParameter("studentName"));
-//            editedUser.setLogin(request.getParameter("studentLogin"));
-//            editedUser.setPassword(request.getParameter("studentPassword"));
-//            editedUser.setEmail(request.getParameter("studentEmail"));
-//            //editedUser.setRole(request.getParameter("studentRole"));
-//            editedUser.getStudentInfo().setState(request.getParameter("studentState"));
             User editedUser = userService.getById(id);
             editedUser.setName(name);
             editedUser.setLogin(login);
@@ -128,24 +95,15 @@ public class InfoPageController {
     }
 
     @Secured({"ROLE_SUPERADMIN", "ROLE_OFFICE", "ROLE_STUDENT"})
-    @RequestMapping(value = "/info/postEducation", method = RequestMethod.POST)
-    public void editEducation(@RequestParam("studentId") Long id,
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @RequestMapping(value = "/postEducation", method = RequestMethod.POST)
+    public String editEducation(@RequestParam("studentId") Long id,
                               @RequestParam("studentUniversity") String university,
                               @RequestParam("studentFaculty") String faculty,
                               @RequestParam("studentCourse") int course,
                               @RequestParam("studentGroup") int group,
-                              @RequestParam("studentGraduationDate") Date graduationDate,
-                              HttpServletResponse response) {
-        try {
-
-//            Map<String, String[]> params = request.getParameterMap();
-//            Set<Map.Entry<String, String[]>> entry = params.entrySet();
-//            for (Map.Entry<String, String[]> element : params.entrySet()) {
-//                System.out.print("Key = " + element.getKey()); //+ ", Value = " + element.getValue()[0]);
-//                for (String s : element.getValue())
-//                    System.out.print("\t" + s);
-//                System.out.println();
-//            }
+                              @RequestParam("studentGraduationDate") Date graduationDate) {
 
             Student editedStudent = studentService.getById(id);
             editedStudent.setUniversity(university);
@@ -155,12 +113,7 @@ public class InfoPageController {
             editedStudent.setGraduationDate(graduationDate);
             studentService.save(editedStudent);
 
-            response.getWriter().print("{\"post\":\"ok\"}"); //string in double quotes
-            response.setStatus(200);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+            return ("{\"post\":\"ok\"}");
     }
 
     @RequestMapping(value = "/redirectInfo")
