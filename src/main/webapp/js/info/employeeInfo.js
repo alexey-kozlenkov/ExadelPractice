024f8d6932172b20389d20f7cc660ba052466682
julia <oblivion522@gmail.com>
2014-08-08 19:41:13 +0300

    refactored code add feedbacks start

---
+++ src/main/webapp/js/info/employeeInfo.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 08.08.2014.
+ */
+var employeeId;
+
+$(window).ready(function () {
+    "use strict";
+    alert(window.location.search);
+    //parseRequestForId(window.location.search);
+    //fillOptions();
+   // fillCommonInfo();
+});
+
+
