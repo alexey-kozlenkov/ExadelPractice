cc4bad7dda086a6cce052115e0f774a62d3b0235
ErmakovVasiliy <vasia-94@tut.by>
2014-08-11 12:12:33 +0300

    code refactor

--- src/main/java/com/exadel/studbase/service/impl/MailServiceImpl.java
+++ src/main/java/com/exadel/studbase/service/impl/MailServiceImpl.java
@@ -23,6 +23,7 @@ public class MailServiceImpl implements IMailService {
     @Override
     public boolean sendMail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
+
         message.setFrom("<" + javaMailSender.getUsername() + ">");
         message.setTo(to);
         message.setSubject(subject);
@@ -38,9 +39,10 @@ public class MailServiceImpl implements IMailService {
 
     @Override
     public boolean sendMail(String from, String password, String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
         javaMailSender.setUsername(from);
         javaMailSender.setPassword(password);
-        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("<" + from + ">");
         message.setTo(to);
         message.setSubject(subject);
