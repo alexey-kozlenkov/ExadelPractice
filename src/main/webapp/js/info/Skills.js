cc40761751107ac987070f6f621f8dc4cd2a0907
ala'n <alan.lonoi@yandex.ru>
2014-08-14 16:56:07 +0300

    Add user make-up changed; Start skill tab

---
+++ src/main/webapp/js/info/Skills.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ala'n on 14.08.2014.
+ */
+define(["jquery", "Util"], function ($, util) {
+    "use strict";
+    function init() {
+
+    }
+    return {
+        init: init
+    };
+});
