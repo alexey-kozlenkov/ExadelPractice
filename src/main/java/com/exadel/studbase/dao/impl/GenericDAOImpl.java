36da6da0461b4875b24ae39d0abe8e6b6d5d231a
alexey kozlenkov <nidecker95@gmail.com>
2014-08-14 10:39:11 +0300

    Add DAOException (2)

--- src/main/java/com/exadel/studbase/dao/impl/GenericDAOImpl.java
+++ src/main/java/com/exadel/studbase/dao/impl/GenericDAOImpl.java
@@ -1,5 +1,6 @@
 package com.exadel.studbase.dao.impl;
 
+import com.exadel.studbase.dao.DAOException;
 import com.exadel.studbase.dao.GenericDAO;
 import com.exadel.studbase.domain.IEntity;
 import com.exadel.studbase.service.filter.Filter;
@@ -17,7 +18,7 @@ import java.util.Collection;
 import java.util.Map;
 
 public abstract class GenericDAOImpl<CONTENT extends IEntity, VIEW extends IEntity, ID extends Serializable>
-        extends HibernateDaoSupport implements GenericDAO<CONTENT, VIEW,  ID> {
+        extends HibernateDaoSupport implements GenericDAO<CONTENT, VIEW, ID> {
 
     private Class<CONTENT> contentClass = (Class<CONTENT>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     private Class<VIEW> viewClass = (Class<VIEW>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
@@ -29,34 +30,55 @@ public abstract class GenericDAOImpl<CONTENT extends IEntity, VIEW extends IEnti
     }
 
     @Override
-    public Collection<CONTENT> getAll() {
-        Collection<CONTENT> result =
-                getSession().createCriteria(contentClass).setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
-        return result;
+    public Collection<CONTENT> getAll() throws DAOException {
+        try {
+            Collection<CONTENT> result =
+                    getSession().createCriteria(contentClass).setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+            return result;
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
     public CONTENT find(ID id) {
-        return (CONTENT) this.getSession().get(contentClass, id);
+        try {
+            CONTENT content = (CONTENT) this.getSession().get(contentClass, id);
+            return content;
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
     public CONTENT saveOrUpdate(CONTENT content) {
-        this.getSession().saveOrUpdate(content);
-        return content;
+        try {
+            this.getSession().saveOrUpdate(content);
+            return content;
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
     public void delete(CONTENT content) {
-        this.getSession().delete(content);
+        try {
+            this.getSession().delete(content);
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
     public Collection<VIEW> getView(Map<String, Filter<VIEW>> filterMap) {
-        Criteria listCriteria = getSession().createCriteria(viewClass);
-        Criterion filterCriterion = FilterUtils.buildFilterCriterion(filterMap);
-        listCriteria.add(filterCriterion);
-        Collection<VIEW> result = listCriteria.list();
-        return result;
+        try {
+            Criteria listCriteria = getSession().createCriteria(viewClass);
+            Criterion filterCriterion = FilterUtils.buildFilterCriterion(filterMap);
+            listCriteria.add(filterCriterion);
+            Collection<VIEW> result = listCriteria.list();
+            return result;
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
     }
 }
