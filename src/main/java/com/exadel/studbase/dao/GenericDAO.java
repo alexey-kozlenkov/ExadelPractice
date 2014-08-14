550432fe6999ddfa819f26f645483ecf78ac2943
alexey kozlenkov <nidecker95@gmail.com>
2014-08-14 10:38:49 +0300

    Add DAOException

--- src/main/java/com/exadel/studbase/dao/GenericDAO.java
+++ src/main/java/com/exadel/studbase/dao/GenericDAO.java
@@ -8,13 +8,13 @@ import java.util.Collection;
 import java.util.Map;
 
 public interface GenericDAO<CONTENT extends IEntity,  VIEW extends IEntity, ID extends Serializable> {
-    Collection<CONTENT> getAll();
+    Collection<CONTENT> getAll() throws DAOException;
 
-    CONTENT find(ID id);
+    CONTENT find(ID id) throws DAOException;
 
-    CONTENT saveOrUpdate(CONTENT content);
+    CONTENT saveOrUpdate(CONTENT content) throws DAOException;
 
-    void delete(CONTENT content);
+    void delete(CONTENT content)throws DAOException;
 
-    Collection<VIEW> getView(Map<String, Filter<VIEW>> filterMap);
+    Collection<VIEW> getView(Map<String, Filter<VIEW>> filterMap)throws DAOException;
 }
